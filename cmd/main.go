9a6fac5403903dde8afd6f167f7d1939c387ec9a
Dima <dima.dorogov1@mail.ru>
2025-02-14 00:55:52 +0300

    First push

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"github.com/dmitrycr/todo-app"
+	"log"
+)
 
+func main() {
+	srv := new(todo.Server)
+	if err := srv.Run("8000"); err != nil {
+		log.Fatalf("error run server: %s", err.Error())
+	}
 }
