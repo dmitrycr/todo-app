b40d6b36986af6ea071d1cd999b08c9eb8fde27a
Dima <dima.dorogov1@mail.ru>
2025-02-15 17:44:40 +0300

    ff

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"github.com/dmitrycr/todo-app"
+	"github.com/dmitrycr/todo-app/pkg/handler"
+	"github.com/dmitrycr/todo-app/pkg/repository"
+	"github.com/dmitrycr/todo-app/pkg/service"
 	"log"
 )
 
 func main() {
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error run server: %s", err.Error())
 	}
 }
